d07b9c1e8916d67b68bff84f284ffd3f1f7f2e86
kw22228 <hot5521@naver.com>
2022-01-18 18:03:38 +0900

    tailwind css, 재귀호출

--- js/hackerNews/app.js
+++ js/hackerNews/app.js
@@ -48,23 +48,23 @@ const getNewsList = () => {
 
     for (let i = (store.currentPage - 1) * list; i < maxList; i++) {
         newsList.push(`
-            <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
-                <div class="flex">
-                    <div class="flex-auto">
-                        <a href="#/show/${newsFeeds[i].id}">${newsFeeds[i].title}</a>
-                    </div>
-                    <div class="text-center text-sm">
-                        <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeeds[i].comments_count}</div>
-                    </div>
-                </div>
-                <div class="flex mt-3">
-                    <div class="grid grid-cols-3 text-sm text-gray-500">
-                        <div><i class="fas fa-user mr-1">${newsFeeds[i].user}</div>
-                        <div><i class="fas fa-heart mr-1">${newsFeeds[i].points}</div>
-                        <div><i class="fas fa-clock mr-1">${newsFeeds[i].time_ago}</div>
-                    </div>
-                </div>
+        <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+            <div class="flex">
+            <div class="flex-auto">
+                <a href="#/show/${newsFeeds[i].id}">${newsFeeds[i].title}</a>  
             </div>
+            <div class="text-center text-sm">
+                <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeeds[i].comments_count}</div>
+            </div>
+            </div>
+            <div class="flex mt-3">
+            <div class="grid grid-cols-3 text-sm text-gray-500">
+                <div><i class="fas fa-user mr-1"></i>${newsFeeds[i].user}</div>
+                <div><i class="fas fa-heart mr-1"></i>${newsFeeds[i].points}</div>
+                <div><i class="far fa-clock mr-1"></i>${newsFeeds[i].time_ago}</div>
+            </div>  
+            </div>
+        </div>    
         `);
     }
     template = template.replace('{{__news_list__}}', newsList.join(''));
@@ -82,17 +82,62 @@ const getNewsList = () => {
 
 const getNewsDetail = () => {
     const hashId = location.hash.substring(7);
-
     const newsContent = getData(CONTENT_URL.replace('@id', hashId));
-    const title = document.createElement('h1');
+    let template = `
+        <div class="bg-gray-600 min-h-screen pb-8">
+            <div class="bg-white text-xl">
+                <div class="mx-auto px-4">
+                <div class="flex justify-between items-center py-6">
+                    <div class="flex justify-start">
+                    <h1 class="font-extrabold">Hacker News</h1>
+                    </div>
+                    <div class="items-center justify-end">
+                    <a href="#/page/${store.currentPage}" class="text-gray-500">
+                        <i class="fa fa-times"></i>
+                    </a>
+                    </div>
+                </div>
+                </div>
+            </div>
 
-    root.innerHTML = `
-        <h1>${newsContent.title}</h1>
+            <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+                <h2>${newsContent.title}</h2>
+                <div class="text-gray-400 h-20">
+                ${newsContent.content}
+                </div>
 
-        <div>
-            <a href="#/page/${store.currentPage}">목록으로</a>
+                {{__comments__}}
+
+            </div>
         </div>
     `;
+
+    const makeComment = (comments, depth = 0) => {
+        const commentStr = [];
+        comments.map(comment => {
+            commentStr.push(`
+                <div style="margin-left: ${depth * 40}px;" class="mt-4">
+                    <div class="text-gray-400">
+                        <i class="fa fa-sort-up mr-2"></i>
+                        <strong>${comment.user}</strong> ${comment.time_ago}
+                    </div>
+                    <p class="text-gray-700">${comment.content}</p>
+                </div>
+            `);
+
+            if (comment.comments.length > 0) {
+                commentStr.push(makeComment(comment.comments, depth + 1));
+            }
+        });
+
+        return commentStr.join('');
+    };
+
+    template = template.replace(
+        '{{__comments__}}',
+        makeComment(newsContent.comments)
+    );
+    root.innerHTML = template;
 };
 
 const router = () => {
--- js/hackerNews/dist/app.c328ef1a.js
+++ js/hackerNews/dist/app.c328ef1a.js
@@ -142,7 +142,7 @@ var getNewsList = function getNewsList() {
   var template = "\n        <div class=\"bg-gray-600 min-h-screen\">\n            <div class=\"bg-white text-xl\">\n                <div class=\"mx-auto px-4\">\n                    <div class=\"flex justify-between items-center py-6\">\n                        <div class=\"flex justify-start\">\n                            <h1 class=\"font-extrabold\">Hacker News</h1>\n                        </div>\n                        <div class=\"items-center justify-end\">\n                            <a href=\"#/page/{{__prev_page__}}\" class=\"text-gray-500\">\n                                Previous\n                            </a>\n                            <a href=\"#/page/{{__next_page__}}\" class=\"text-gray-500 ml-4\">\n                                Next\n                            </a>\n                        </div>\n                    </div>\n                </div>\n            </div>\n            <div class=\"p-4 text-2xl text-gray-700\">\n                {{__news_list__}}\n            </div>\n        </div>\n    ";
 
   for (var i = (store.currentPage - 1) * list; i < maxList; i++) {
-    newsList.push("\n            <div class=\"p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100\">\n                <div class=\"flex\">\n                    <div class=\"flex-auto\">\n                        <a href=\"#/show/".concat(newsFeeds[i].id, "\">").concat(newsFeeds[i].title, "</a>\n                    </div>\n                    <div class=\"text-center text-sm\">\n                        <div class=\"w-10 text-white bg-green-300 rounded-lg px-0 py-2\">").concat(newsFeeds[i].comments_count, "</div>\n                    </div>\n                </div>\n                <div class=\"flex mt-3\">\n                    <div class=\"grid grid-cols-3 text-sm text-gray-500\">\n                        <div><i class=\"fas fa-user mr-1\">").concat(newsFeeds[i].user, "</div>\n                        <div><i class=\"fas fa-heart mr-1\">").concat(newsFeeds[i].points, "</div>\n                        <div><i class=\"fas fa-clock mr-1\">").concat(newsFeeds[i].time_ago, "</div>\n                    </div>\n                </div>\n            </div>\n        "));
+    newsList.push("\n        <div class=\"p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100\">\n            <div class=\"flex\">\n            <div class=\"flex-auto\">\n                <a href=\"#/show/".concat(newsFeeds[i].id, "\">").concat(newsFeeds[i].title, "</a>  \n            </div>\n            <div class=\"text-center text-sm\">\n                <div class=\"w-10 text-white bg-green-300 rounded-lg px-0 py-2\">").concat(newsFeeds[i].comments_count, "</div>\n            </div>\n            </div>\n            <div class=\"flex mt-3\">\n            <div class=\"grid grid-cols-3 text-sm text-gray-500\">\n                <div><i class=\"fas fa-user mr-1\"></i>").concat(newsFeeds[i].user, "</div>\n                <div><i class=\"fas fa-heart mr-1\"></i>").concat(newsFeeds[i].points, "</div>\n                <div><i class=\"far fa-clock mr-1\"></i>").concat(newsFeeds[i].time_ago, "</div>\n            </div>  \n            </div>\n        </div>    \n        "));
   }
 
   template = template.replace('{{__news_list__}}', newsList.join(''));
@@ -154,8 +154,23 @@ var getNewsList = function getNewsList() {
 var getNewsDetail = function getNewsDetail() {
   var hashId = location.hash.substring(7);
   var newsContent = getData(CONTENT_URL.replace('@id', hashId));
-  var title = document.createElement('h1');
-  root.innerHTML = "\n        <h1>".concat(newsContent.title, "</h1>\n\n        <div>\n            <a href=\"#/page/").concat(store.currentPage, "\">\uBAA9\uB85D\uC73C\uB85C</a>\n        </div>\n    ");
+  var template = "\n        <div class=\"bg-gray-600 min-h-screen pb-8\">\n            <div class=\"bg-white text-xl\">\n                <div class=\"mx-auto px-4\">\n                <div class=\"flex justify-between items-center py-6\">\n                    <div class=\"flex justify-start\">\n                    <h1 class=\"font-extrabold\">Hacker News</h1>\n                    </div>\n                    <div class=\"items-center justify-end\">\n                    <a href=\"#/page/".concat(store.currentPage, "\" class=\"text-gray-500\">\n                        <i class=\"fa fa-times\"></i>\n                    </a>\n                    </div>\n                </div>\n                </div>\n            </div>\n\n            <div class=\"h-full border rounded-xl bg-white m-6 p-4 \">\n                <h2>").concat(newsContent.title, "</h2>\n                <div class=\"text-gray-400 h-20\">\n                ").concat(newsContent.content, "\n                </div>\n\n                {{__comments__}}\n\n            </div>\n        </div>\n    ");
+
+  var makeComment = function makeComment(comments) {
+    var depth = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+    var commentStr = [];
+    comments.map(function (comment) {
+      commentStr.push("\n                <div style=\"margin-left: ".concat(depth * 40, "px;\" class=\"mt-4\">\n                    <div class=\"text-gray-400\">\n                        <i class=\"fa fa-sort-up mr-2\"></i>\n                        <strong>").concat(comment.user, "</strong> ").concat(comment.time_ago, "\n                    </div>\n                    <p class=\"text-gray-700\">").concat(comment.content, "</p>\n                </div>\n            "));
+
+      if (comment.comments.length > 0) {
+        commentStr.push(makeComment(comment.comments, depth + 1));
+      }
+    });
+    return commentStr.join('');
+  };
+
+  template = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+  root.innerHTML = template;
 };
 
 var router = function router() {
