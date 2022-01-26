fd61b4cd0f1d5031c823a9907ac36a92bbc3787d
kw22228 <hot5521@naver.com>
2022-01-26 18:34:13 +0900

    view class

---
+++ ts/hackerNews/src/core/view.ts
@@ -0,0 +1,46 @@
+//View Classes
+export default abstract class View {
+    private template: string; //원본
+    private renderTemplate: string;
+    private root: HTMLElement;
+    private htmlArr: string[];
+
+    constructor(rootId: string, template: string) {
+        const rootElement = document.getElementById(rootId);
+
+        if (!rootElement) {
+            throw 'Undefined root Element';
+        }
+
+        this.root = rootElement;
+        this.template = template;
+        this.renderTemplate = template;
+        this.htmlArr = [];
+    }
+
+    protected updateView(): void {
+        this.root.innerHTML = this.renderTemplate;
+        this.renderTemplate = this.template;
+    }
+
+    protected setTemplate(key: string, value: string): void {
+        this.renderTemplate = this.renderTemplate.replace(`{{__${key}__}}`, value);
+    }
+
+    protected addHtml(htmlStr: string): void {
+        this.htmlArr.push(htmlStr);
+    }
+
+    protected getHtml(): string {
+        const snapshot: string = this.htmlArr.join('');
+        this.clearHtmlArr();
+
+        return snapshot;
+    }
+
+    private clearHtmlArr(): void {
+        this.htmlArr = [];
+    }
+
+    abstract render(): void;
+}
