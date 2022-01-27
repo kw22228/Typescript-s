798dbe9e62a67f913c2d833ab368caf3b430fda7
kw22228 <hot5521@naver.com>
2022-01-27 17:55:26 +0900

    router update

--- ts/hackerNews/src/core/router.ts
+++ ts/hackerNews/src/core/router.ts
@@ -2,39 +2,61 @@ import { RouteInfo } from '../types';
 import View from './view';
 
 export default class Router {
+    private isStart: boolean;
     private routeTable: RouteInfo[];
     private defaultRoute: RouteInfo | null;
+
     constructor() {
         window.addEventListener('hashchange', this.route.bind(this));
 
+        this.isStart = false;
         this.routeTable = [];
         this.defaultRoute = null;
     }
 
-    setDefaultPage(page: View): void {
+    setDefaultPage(page: View, params: RegExp | null = null): void {
         this.defaultRoute = {
             path: '',
             page,
+            params,
         };
     }
 
-    addRoutePath(path: string, page: View): void {
+    addRoutePath(path: string, page: View, params: RegExp | null = null): void {
         this.routeTable.push({
             path, //프로퍼티의 스키마와 값이 같은경우 생략
             page,
+            params,
         });
+
+        if (!this.isStart) {
+            this.isStart = true;
+
+            setTimeout(this.route.bind(this), 0);
+        }
     }
 
     route() {
-        const routePath = location.hash;
+        const routePath: string = location.hash;
+
         if (routePath === '' && this.defaultRoute) {
             this.defaultRoute.page.render();
+            return;
         }
 
-        for (const route of this.routeTable) {
-            if (routePath.indexOf(route.path) >= 0) {
-                route.page.render();
-                break;
+        for (const routeInfo of this.routeTable) {
+            if (routePath.indexOf(routeInfo.path) >= 0) {
+                if (routeInfo.params) {
+                    const parseParams = routePath.match(routeInfo.params);
+
+                    if (parseParams) {
+                        routeInfo.page.render.apply(null, [parseParams[1]]);
+                    }
+                } else {
+                    routeInfo.page.render();
+                }
+
+                return;
             }
         }
     }
