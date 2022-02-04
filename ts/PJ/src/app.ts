bb0325d38777b861bfd15a40d70253b98b854b0a
kw22228 <hot5521@naver.com>
2022-02-04 15:34:10 +0900

    textfield 추가

--- ts/PJ/src/app.ts
+++ ts/PJ/src/app.ts
@@ -1,5 +1,6 @@
 import template from './app.template';
 import { AnyObject } from './types';
+import TextField from './views/text-field';
 export default class App {
     template = template;
     container: HTMLElement;
@@ -14,9 +15,40 @@ export default class App {
         this.init();
     }
 
-    private init = () => {};
+    private init = () => {
+        const nameField = new TextField('#required-fields', {
+            id: 'name',
+            label: '이름',
+            type: 'text',
+            placeholder: '이름을 입력해주세요.',
+            require: true,
+        });
+
+        const idField = new TextField('#required-fields', {
+            id: 'id',
+            label: '아이디',
+            type: 'text',
+            placeholder: '아이디를 입력해주세요.',
+            require: true,
+        });
+
+        const emailField = new TextField('#required-fields', {
+            id: 'email',
+            label: '이메일',
+            type: 'email',
+            placeholder: '이메일을 입력해주세요',
+            require: true,
+        });
+
+        this.fields.push(nameField);
+        this.fields.push(idField);
+        this.fields.push(emailField);
+    };
 
     render = () => {
         this.container.innerHTML = this.template(this.data);
+        this.fields.map(field => {
+            field.render(true);
+        });
     };
 }
