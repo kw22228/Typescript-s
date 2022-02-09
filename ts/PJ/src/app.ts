5fd19a12fc95952d04433fda30b1843eadb88a19
kw22228 <hot5521@naver.com>
2022-02-09 17:48:59 +0900

    submit button monitor

--- ts/PJ/src/app.ts
+++ ts/PJ/src/app.ts
@@ -1,11 +1,14 @@
 import template from './app.template';
+import { CantContainWhitespace, CantStartNumber, MinimumLengthLimit } from './constant';
 import { AnyObject } from './types';
 import { TextField, PasswordField } from './views/';
+import AddressField from './views/address-field';
 export default class App {
     template = template;
     container: HTMLElement;
     data: AnyObject;
     fields: AnyObject[];
+    active: boolean = false;
 
     constructor(container: string, data: AnyObject = {}) {
         this.container = document.querySelector(container) as HTMLElement;
@@ -13,6 +16,9 @@ export default class App {
         this.fields = [];
 
         this.init();
+
+        // setInterval(this.validFieldMonitor, 1000 / 30);
+        setTimeout(this.validFieldMonitor, 0);
     }
 
     private init = () => {
@@ -46,10 +52,40 @@ export default class App {
             placeholder: '비밀번호를 입력해주세요.',
         });
 
+        const addressField = new AddressField('#required-fields', {
+            id: 'address',
+            label: '배송지 주소',
+        });
+
+        idField.addValidateRule(CantContainWhitespace);
+        idField.addValidateRule(CantStartNumber);
+        idField.addValidateRule(MinimumLengthLimit(3));
+
+        emailField.addValidateRule(CantContainWhitespace);
+
         this.fields.push(nameField);
         this.fields.push(idField);
         this.fields.push(emailField);
         this.fields.push(passwordField);
+        this.fields.push(addressField);
+    };
+
+    private validFieldMonitor = () => {
+        const btnJoin = this.container.querySelector('#btn-join') as HTMLButtonElement;
+
+        const valid = this.fields.filter(field => {
+            return field.isValid;
+        });
+
+        if (valid.length === this.fields.length) {
+            this.active = true;
+            btnJoin.classList.remove('bg-gray-300');
+            btnJoin.classList.add('bg-green-500');
+        } else {
+            this.active = false;
+            btnJoin.classList.remove('bg-green-500');
+            btnJoin.classList.add('bg-gray-300');
+        }
     };
 
     render = () => {
@@ -57,5 +93,7 @@ export default class App {
         this.fields.map(field => {
             field.render(true);
         });
+
+        this.container.addEventListener('submit', (e: Event) => {});
     };
 }
