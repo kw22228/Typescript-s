85a68c90a159f2ba7c82fbb1fff0e95b90bfe7c4
김재원 <kw2282222@gmail.com>
2022-02-13 20:40:51 +0900

    user type

---
+++ ts/PJ/src/types/user.ts
@@ -0,0 +1,10 @@
+import { Address } from './address';
+type User = {
+    name: string;
+    id: string;
+    email: string;
+    password: string;
+    address?: Address;
+};
+
+export default User;
