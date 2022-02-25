d82e0fdf5e1fcdf72ab1ecc2001627bb2e5ba289
김재원 <kw2282222@gmail.com>
2022-02-25 17:08:44 +0900

    login user, user post

--- Login/server/index.js
+++ Login/server/index.js
@@ -41,3 +41,45 @@ app.get('/api/user/:id', (req, res) => {
             });
         });
 });
+
+app.get('/api/user/:id/posts', (req, res) => {
+    if (!req.headers['token']) {
+        return res.status(403).send({
+            status: 'Error',
+        });
+    }
+
+    axios
+        .get(`https://jsonplaceholder.typicode.com/posts?userId=${req.params.id}`)
+        .then(result => {
+            res.status(200).send({
+                status: 'ok',
+                result: result.data,
+            });
+        })
+        .catch(e => {
+            res.status(400).send({
+                status: 'Error',
+            });
+        });
+});
+
+app.post('api/authentication', (req, res) => {
+    if (Math.floor(Math.random() * 10) % 2 === 0) {
+        res.status(200).send({
+            status: 'ok',
+            result: {
+                id: Math.floor(Math.random() * 10),
+                token: v4(),
+            },
+        });
+    } else {
+        res.status(400).send({
+            status: 'Error',
+        });
+    }
+});
+
+app.listen(8080, () => {
+    console.log('ready to dumy login server');
+});
