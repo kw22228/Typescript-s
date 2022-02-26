1e12ebff8e33a326baf498b021bccb92973840b2
김재원 <kw2282222@gmail.com>
2022-02-26 17:56:43 +0900

    view

---
+++ ts/Login/src/views/core.js
@@ -0,0 +1,34 @@
+const DefaultProps = {
+    id: '',
+    text: '',
+    label: 'label',
+    type: 'text',
+    placeholder: '',
+    require: false,
+};
+
+export default class CoreField {
+    _template;
+    _container;
+    _data;
+    _validateRules = [];
+
+    constructor(template, container, data) {
+        this._template = template;
+        this._container = container;
+        this._data = { ...DefaultProps, ...data };
+    }
+
+    render = (append = false) => {
+        const container = document.querySelector(this._container);
+
+        if (append) {
+            const div = document.createElement('div');
+            div.innerHTML = this._template(this.buildData());
+
+            container.appendChild(div.children[0]);
+        } else {
+            container.innerHTML = this._template(this.buildData());
+        }
+    };
+}
---
+++ ts/Login/src/views/text-field.js
@@ -0,0 +1,6 @@
+import template from './text-field.template';
+export default class TextField {
+    constructor(container, data) {
+        super(template, container, data);
+    }
+}
