b5fdfefcd7aecb77e270c49e2803e209e74bf4b8
김재원 <kw2282222@gmail.com>
2022-02-26 20:16:52 +0900

    textfield render

--- ts/Login/src/views/text-field.js
+++ ts/Login/src/views/text-field.js
@@ -1,6 +1,24 @@
 import template from './text-field.template';
-export default class TextField {
+import CoreField from './core';
+export default class TextField extends CoreField {
     constructor(container, data) {
         super(template, container, data);
     }
+
+    buildData = () => {
+        return {
+            ...this._data,
+        };
+    };
+
+    // render = (append = false) => {
+    //     const container = document.querySelector(this._container);
+
+    //     if (append) {
+    //         const div = document.createElement('div');
+    //         div.innerHTML = this._template(this.buildData());
+
+    //         container.appendChild(div.children[0]);
+    //     }
+    // };
 }
