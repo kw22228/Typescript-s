c97b6b6164549404a42775ad3dfe4c7e30c2723a
김재원 <kw2282222@gmail.com>
2022-02-26 20:17:33 +0900

    id, password field

--- ts/Login/src/page/login.js
+++ ts/Login/src/page/login.js
@@ -1,13 +1,50 @@
+import TextField from '../views/text-field';
 import template from './login.template';
+
 export default class Login {
     _template = template;
     _container;
     _data;
+    _fields = [];
 
     constructor(container, data) {
         this._container = document.querySelector(container);
         this._data = data;
+
+        this.init();
     }
 
-    init = () => {};
+    init = () => {
+        console.log('login initial');
+
+        const idField = new TextField('#login-fields', {
+            id: 'userid',
+            label: '아이디',
+            type: 'text',
+            placeholder: '아이디를 입력해주세요.',
+            require: true,
+        });
+        const passwordField = new TextField('#login-fields', {
+            id: 'password',
+            label: '패스워드',
+            type: 'password',
+            placeholder: '패스워드를 입력해주세요.',
+            require: true,
+        });
+
+        this._fields.push(idField);
+        this._fields.push(passwordField);
+    };
+
+    render = () => {
+        console.log('Login render');
+
+        this._container.innerHTML = this._template({
+            ...this._data,
+        });
+
+        this._fields.map(field => {
+            field.render(true);
+        });
+    };
 }
